9959762ae0487ad82356311aed7b2109458d1f8c
Suthar, Bhavin Udavji <bhavinathunt@gmail.com>
2022-01-27 21:59:15 +0530

    link update

--- crowdcoin/smart_contract/factory.js
+++ crowdcoin/smart_contract/factory.js
@@ -1,5 +1,5 @@
 import web3 from './web3';
-const CampaignFactory =  require('../../Contract/build/contracts/CampaignFactory.json');
+const CampaignFactory =  require('./CampaignFactory.json');
 const maticAddress = '0x88DB67772d05b37e5B0A1e9Ba044E21bfcD0eb81'
 const ropstanAddress = '0xA9547144e9B517198401C84B8Adc07b2020a818C'
 const instance = new web3.eth.Contract(
