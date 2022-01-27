5db1644d7f32fc1f9016aa62370b6c30ea9777f6
Suthar, Bhavin Udavji <bhavinathunt@gmail.com>
2022-01-27 18:13:37 +0530

    Few changes

--- crowdcoin/server.js
+++ crowdcoin/server.js
@@ -6,11 +6,11 @@ const next = require('next')
 const routes = require('./router');
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000;
 const app = next({ dev })
 const handler = routes.getRequestHandler(app)
 const handle = app.getRequestHandler()
 
-var port = Number(process.env.PORT || 3000)
 app.prepare().then(() => {
   createServer(handler).listen(port, (err) => {
     if (err) throw err
